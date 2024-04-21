d0ddf49e9b6dacea8b4dbaad60701dae83f10ec4
Thisara <sasmithajayasinghe1@gmail.com>
2024-04-21 23:54:29 +0530

    add new book

--- src/main/java/com/bookStore/bookStore/controller/BookController.java
+++ src/main/java/com/bookStore/bookStore/controller/BookController.java
@@ -1,10 +1,18 @@
 package com.bookStore.bookStore.controller;
 
+import com.bookStore.bookStore.entity.Book;
+import com.bookStore.bookStore.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class BookController {
+
+    @Autowired
+    BookService bookService;
 
     @GetMapping("/")
     public String home(){
@@ -19,5 +27,11 @@ public class BookController {
     @GetMapping("/available_books")
     public String getAllBook(){
         return "bookList";
+    }
+
+    @PostMapping("/save")
+    public String addBook(@ModelAttribute Book b){
+        bookService.save(b);
+        return "redirect:/available_books";
     }
 }
---
+++ src/main/java/com/bookStore/bookStore/service/BookService.java
@@ -0,0 +1,17 @@
+package com.bookStore.bookStore.service;
+
+import com.bookStore.bookStore.entity.Book;
+import com.bookStore.bookStore.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookService {
+
+    @Autowired
+    private BookRepository bRepo;
+
+    public void save(Book b){
+        bRepo.save(b);
+    }
+}
